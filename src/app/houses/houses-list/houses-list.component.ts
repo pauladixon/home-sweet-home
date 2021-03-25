7fab2b798b7cd6ac1e8274a1e3bbbeb295b0c48f
paula dixon <paulapdixon@gmail.com>
2021-03-25 09:14:22 -0700

    added houses list component

---
+++ src/app/houses/houses-list/houses-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-houses-list',
+  templateUrl: './houses-list.component.html',
+  styleUrls: ['./houses-list.component.sass']
+})
+export class HousesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/houses/houses.module.ts
+++ src/app/houses/houses.module.ts
@@ -1,11 +1,11 @@
 import { HousesComponent } from './houses.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousesListComponent } from './houses-list/houses-list.component';
 
 @NgModule({
-  declarations: [ HousesComponent ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ HousesComponent, HousesListComponent ],
+  imports: [ CommonModule ],
+  exports: [ HousesComponent ]
 })
 export class HousesModule { }
