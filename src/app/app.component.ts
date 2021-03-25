e9e4203b4b1acbf7cfb8113df98725f4c637e4bd
paula dixon <paulapdixon@gmail.com>
2021-03-25 08:42:37 -0700

    added houses component and removed test files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'home-sweet-home';
+export class AppComponent implements OnInit {
+
+
+  title = 'home-sweet-home'
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HousesComponent } from './houses/houses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HousesComponent
   ],
   imports: [
     BrowserModule,
